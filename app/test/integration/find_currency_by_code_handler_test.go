f83c3888cce3725f099bceb76d3d8a1e5847a1b2
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-09-25 21:25:12 -0300

    feat: find currency by code handler test

--- app/test/integration/find_currency_by_code_handler_test.go
+++ app/test/integration/find_currency_by_code_handler_test.go
@@ -1,47 +1,40 @@
 package integration
 
 import (
-	"context"
 	"encoding/json"
 	"io"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/wesleyfebarretos/challenge-bravo/app/internal/infra/repository/currency_repository"
 	"github.com/wesleyfebarretos/challenge-bravo/app/internal/infra/web/handler"
 	"github.com/wesleyfebarretos/challenge-bravo/app/test/testdata"
 	"github.com/wesleyfebarretos/challenge-bravo/app/test/testutils"
 )
 
-func TestFindAllCurrencyHandler(t *testing.T) {
-	t.Run("it should find all currencies", testutils.RunTest(func(t *testing.T) {
-		currencies, err := currency_repository.New().FindAll(context.TODO())
+func TestFindCurrencyByCodeHandler(t *testing.T) {
+	t.Run("it should find a currency by code", testutils.RunTest(func(t *testing.T) {
+		user, err := testdata.CreateUser()
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		oldLen := len(currencies)
-
-		_, err = testdata.CreateCurrency()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		res := testutils.SendRequest(t, http.MethodGet, "currency", nil)
+		res := testutils.SendRequestWithToken(t, http.MethodGet, "currency/code/USD", user, nil)
 
 		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		expectedResponse := []handler.FindAllCurrencyHandler{}
+		expectedResponse := handler.FindCurrencyByCodeResponse{}
 
 		if err := json.Unmarshal(body, &expectedResponse); err != nil {
 			t.Fatal(err)
 		}
 
 		assert.Equal(t, http.StatusOK, res.StatusCode)
-		assert.Equal(t, oldLen+1, len(expectedResponse))
+		assert.Equal(t, "USD", expectedResponse.Code)
+		assert.Equal(t, 1, expectedResponse.ID)
+		assert.Equal(t, float64(1), expectedResponse.USDExchangeRate)
 	}))
 }
